b0a59085ecc8dccb37089ff6aaab241cf3c7c646
Tyuiu.LomskinVI@outlook.com <winr2005@gmail.com>
2023-09-11 22:13:17 +0500

    Task5

--- Tyuiu.LomakinVI.Sprint0.Task5.V0.Test/DataServiceTest.cs
+++ Tyuiu.LomakinVI.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using Tyuiu.LomakinVI.Sprint0.Task4.V0.Lib;
+using Tyuiu.LomakinVI.Sprint0.Task5.V0.Lib;
 
-namespace Tyuiu.LomakinVI.Sprint0.Task4.V0.Test
+namespace Tyuiu.LomakinVI.Sprint0.Task5.V0.Test
 {
     [TestClass]
     public class DataServiceTest
